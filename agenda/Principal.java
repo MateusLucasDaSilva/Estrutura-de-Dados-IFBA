d2202af0ec721b5171cfe2723d49efac7a1dd895
Mateus Lucas <101568163+MateusLucasDaSilva@users.noreply.github.com>
2022-04-15 07:22:21 -0300

    Add files via upload

---
+++ agenda/Principal.java
@@ -0,0 +1,27 @@
+
+
+package agenda;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Servico s = new Servico();
+		Scanner in = new Scanner(System.in);
+		s.adicionar();
+		s.listar();
+		//s.procurar();
+		//System.out.println("Digite em qual posição voçê deseja incerir uma nova conta");
+		//int aux = in.nextInt();
+		//s.Aumentar(aux);
+        //s.listar();
+        System.out.println("Digite em qual posição voçê deseja excluir");
+		int aux2 = in.nextInt();
+		s.excluir(aux2);
+		s.listar();
+
+	}
+}
+
+
