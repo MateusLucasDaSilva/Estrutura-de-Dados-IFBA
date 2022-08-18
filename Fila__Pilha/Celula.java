690c844e02a7c041eabe4e1667a2f96a8b823c49
Mateus Lucas <101568163+MateusLucasDaSilva@users.noreply.github.com>
2022-08-18 13:59:53 +1100

    Add files via upload

---
+++ Fila__Pilha/Celula.java
@@ -0,0 +1,30 @@
+package Fila__Pilha;
+
+public class Celula {
+	
+	private String nome;
+
+	public Celula() {
+	}
+
+	public Celula(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "" + nome + "";
+	}
+	
+	
+	
+
+}
